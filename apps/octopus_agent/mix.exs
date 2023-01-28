c35651731eff3968e8d3aaf768e7c9ba685cdc73
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2023-01-28 15:35:27 +0100

    Set version to 0.3.0

--- apps/octopus_agent/mix.exs
+++ apps/octopus_agent/mix.exs
@@ -4,7 +4,7 @@ defmodule OctopusAgent.MixProject do
   def project do
     [
       app: :octopus_agent,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.14",
       description: description(),
       package: package(),
