07d41a36dac3cae8ac3f826b613578ddfffc8d2e
Anton Mishchuk <anton.mishchuk@kloeckner.com>
2023-05-25 14:14:42 +0200

    Version 0.4.3

--- apps/octopus/mix.exs
+++ apps/octopus/mix.exs
@@ -4,7 +4,7 @@ defmodule Octopus.MixProject do
   def project do
     [
       app: :octopus,
-      version: "0.4.2",
+      version: "0.4.3",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
